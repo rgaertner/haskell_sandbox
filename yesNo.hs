32fb825d2b94789d540f902270da836c4aa90b5d
rgaertner <rgaertner@frysbox.de>
2012-07-24 22:26:12 +0200

    add yesnoIf function

--- yesNo.hs
+++ yesNo.hs
@@ -27,4 +27,5 @@ instance YesNo TrafficLight where
 	yesno Red = False
 	yesno _ = True
 
-
+yesnoIf :: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult
